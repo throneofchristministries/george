27de9a3754047cbe2c076fb78cc9dbcfa3883f8b
throneofchristministries <throneofchristministries2020@gmail.com>
2026-02-03 09:36:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // STEP 1: PASTE YOUR FIREBASE CONFIG HERE
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyBVkn_LvrbOFKmZLoJxMsrZXqugUQvml74",
-authDomain: "throneofchristministries-6a88a.firebaseapp.com",
-projectId: "throneofchristministries-6a88a",
+  apiKey: "AIzaSyBVkn_LvrbOFKmZLoJxMsrZXqugUQvml74",
+  authDomain: "throneofchristministries-6a88a.firebaseapp.com",
+  projectId: "throneofchristministries-6a88a",
+  storageBucket: "throneofchristministries-6a88a.firebasestorage.app",
+  messagingSenderId: "970066493474",
+  appId: "1:970066493474:web:99d8f66402206b90998a7b"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
